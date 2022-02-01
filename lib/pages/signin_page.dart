94fecda0bb6bb87d4a0d40c6494dba55eabc21d9
Joshua Davian <joshuadavian@gmail.com>
2022-02-01 20:31:43 +0700

    signin page - title

--- lib/pages/signin_page.dart
+++ lib/pages/signin_page.dart
@@ -1,10 +1,66 @@
 import 'package:flutter/material.dart';
+import 'package:space/theme.dart';
 
 class SigninPage extends StatelessWidget {
   const SigninPage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: ListView(
+        padding: EdgeInsets.symmetric(
+          horizontal: 24,
+        ),
+        children: [
+          title(),
+        ],
+      ),
+    );
+  }
+
+  Widget title() {
+    return Container(
+      margin: EdgeInsets.only(
+        top: 84,
+      ),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text(
+            'Login to your\naccount',
+            style: blackTextStyle.copyWith(
+              fontSize: 24,
+              fontWeight: bold,
+            ),
+          ),
+          SizedBox(
+            height: 20,
+          ),
+          Row(
+            children: [
+              Container(
+                width: 87,
+                height: 4,
+                margin: EdgeInsets.only(
+                  right: 4,
+                ),
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(4),
+                  color: kBlackColor,
+                ),
+              ),
+              Container(
+                width: 8,
+                height: 4,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(4),
+                  color: kBlackColor,
+                ),
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
   }
 }

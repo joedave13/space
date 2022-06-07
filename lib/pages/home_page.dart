9ddb639b015cc0963aeb7be005c31541c7a7bfe5
Joshua Davian <joshuadavian@gmail.com>
2022-06-07 13:52:58 +0700

    home page - header & search bar

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -42,10 +42,78 @@ class HomePage extends StatelessWidget {
           ],
         ),
       ),
-      body: Center(
-        child: Text(
-          'Home Page',
-        ),
+      body: Stack(
+        children: [
+          Image.asset(
+            'assets/image_background.png',
+          ),
+          ListView(
+            children: [
+              // TITLE
+              Container(
+                margin: EdgeInsets.only(
+                  top: 24,
+                ),
+                padding: EdgeInsets.symmetric(
+                  horizontal: 24,
+                ),
+                child: Row(
+                  children: [
+                    Image.asset(
+                      'assets/logo_dark.png',
+                      width: 54,
+                    ),
+                    SizedBox(
+                      width: 8,
+                    ),
+                    Text(
+                      'Space',
+                      style: blackTextStyle.copyWith(
+                        fontSize: 19,
+                        fontWeight: bold,
+                      ),
+                    ),
+                    Spacer(),
+                    Image.asset(
+                      'assets/icon_cart.png',
+                      width: 30,
+                    ),
+                  ],
+                ),
+              ),
+              // SEARCH BAR
+              Container(
+                margin: EdgeInsets.only(
+                  top: 30,
+                  left: 24,
+                  right: 24,
+                ),
+                padding: EdgeInsets.all(16),
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(14),
+                  color: kWhiteColor,
+                ),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text(
+                      'Search Funiture',
+                      style: grayTextStyle.copyWith(
+                        fontSize: 16,
+                        fontWeight: semiBold,
+                      ),
+                    ),
+                    Image.asset(
+                      'assets/icon_search.png',
+                      width: 24,
+                      color: kGrayColor,
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ],
       ),
     );
   }

536aef0ccc8de30256e999ba185dc1ac143b2b3f
Joshua Davian <joshuadavian@gmail.com>
2022-07-25 22:53:00 +0700

    category page

--- lib/widgets/home_category_item.dart
+++ lib/widgets/home_category_item.dart
@@ -15,55 +15,60 @@ class HomeCategoryItem extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      height: 123,
-      width: MediaQuery.of(context).size.width - (2 * 24),
-      child: Stack(
-        children: [
-          Align(
-            alignment: Alignment.bottomCenter,
-            child: Container(
-              height: 102,
-              decoration: BoxDecoration(
-                color: kWhiteColor,
-                borderRadius: BorderRadius.circular(14),
-              ),
-              child: Row(
-                children: [
-                  SizedBox(
-                    width: 150,
-                  ),
-                  Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Text(
-                        title,
-                        style: blackTextStyle.copyWith(
-                          fontSize: 18,
-                          fontWeight: semiBold,
+    return GestureDetector(
+      onTap: () {
+        Navigator.pushNamed(context, '/category');
+      },
+      child: Container(
+        height: 123,
+        width: MediaQuery.of(context).size.width - (2 * 24),
+        child: Stack(
+          children: [
+            Align(
+              alignment: Alignment.bottomCenter,
+              child: Container(
+                height: 102,
+                decoration: BoxDecoration(
+                  color: kWhiteColor,
+                  borderRadius: BorderRadius.circular(14),
+                ),
+                child: Row(
+                  children: [
+                    SizedBox(
+                      width: 150,
+                    ),
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Text(
+                          title,
+                          style: blackTextStyle.copyWith(
+                            fontSize: 18,
+                            fontWeight: semiBold,
+                          ),
                         ),
-                      ),
-                      SizedBox(
-                        height: 4,
-                      ),
-                      Text(
-                        subtitle,
-                        style: grayTextStyle.copyWith(
-                          fontWeight: semiBold,
+                        SizedBox(
+                          height: 4,
                         ),
-                      ),
-                    ],
-                  )
-                ],
+                        Text(
+                          subtitle,
+                          style: grayTextStyle.copyWith(
+                            fontWeight: semiBold,
+                          ),
+                        ),
+                      ],
+                    )
+                  ],
+                ),
               ),
             ),
-          ),
-          Image.asset(
-            imageUrl,
-            width: 122,
-          ),
-        ],
+            Image.asset(
+              imageUrl,
+              width: 122,
+            ),
+          ],
+        ),
       ),
     );
   }

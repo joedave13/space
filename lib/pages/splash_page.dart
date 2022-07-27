8b78d5ad8b597d1862a0e878f38e6597156bd43c
Joshua Davian <joshuadavian@gmail.com>
2022-07-27 23:51:31 +0700

    splash screen - lottie animation

--- lib/pages/splash_page.dart
+++ lib/pages/splash_page.dart
@@ -2,6 +2,7 @@ import 'dart:async';
 
 import 'package:flutter/material.dart';
 import 'package:space/theme.dart';
+import 'package:lottie/lottie.dart';
 
 class SplashPage extends StatefulWidget {
   const SplashPage({Key? key}) : super(key: key);
@@ -13,7 +14,10 @@ class SplashPage extends StatefulWidget {
 class _SplashPageState extends State<SplashPage> {
   @override
   void initState() {
-    Timer(Duration(seconds: 3), () {
+    Timer(
+        Duration(
+          milliseconds: 2000,
+        ), () {
       Navigator.pushNamedAndRemoveUntil(
         context,
         '/onboarding',
@@ -28,31 +32,8 @@ class _SplashPageState extends State<SplashPage> {
     return Scaffold(
       backgroundColor: kBlackColor,
       body: Center(
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Container(
-              width: 98,
-              height: 82,
-              margin: EdgeInsets.only(
-                right: 16,
-              ),
-              decoration: BoxDecoration(
-                image: DecorationImage(
-                  image: AssetImage(
-                    'assets/logo_light.png',
-                  ),
-                ),
-              ),
-            ),
-            Text(
-              'Space',
-              style: whiteTextStyle.copyWith(
-                fontSize: 36,
-                fontWeight: bold,
-              ),
-            ),
-          ],
+        child: Lottie.asset(
+          'assets/c-space-animation.json',
         ),
       ),
     );
